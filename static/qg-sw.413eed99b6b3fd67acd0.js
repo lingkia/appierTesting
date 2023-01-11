7134ed79e33670c2020610a2d7d39bb9134937b6
Lick_Yan <lickyan123@gmail.com>
2023-01-11 15:13:18 +0800

    ssss

--- static/qg-sw.413eed99b6b3fd67acd0.js
+++ static/qg-sw.413eed99b6b3fd67acd0.js
@@ -1,4 +1,2 @@
 var appId = "413eed99b6b3fd67acd0";
-var worker = new Worker("https://cdn.qgraph.io/v3/r/qg-sw.js");
-// importScripts("https://cdn.qgraph.io/v3/r/qg-sw.js");
-worker.postMessage();
+importScripts("https://cdn.qgraph.io/v3/r/qg-sw.js");
